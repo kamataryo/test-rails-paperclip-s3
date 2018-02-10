e6952d787fddff34257609e4f62b554f72ce80e7
kamataryo <mugil.cephalus+ciconia.local@gmail.com>
2018-02-10 15:17:14 +0900

    Add top index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'top#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resource :images
@@ -7,5 +8,4 @@ Rails.application.routes.draw do
       get 'show_image'
     end
   end
-
 end
